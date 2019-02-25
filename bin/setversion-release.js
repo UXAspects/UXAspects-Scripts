8c150bc657c88ea8ffa7c28eb9e3ef4162cebd24
donna-marijne <donna.worby@hpe.com>
2019-02-25 17:06:34 +0000

    Refactoring scripts for quantum-ux-aspects
    * Added package-artifactory script which is adapted from the one in ux-aspects-darwin.
    * Added command line args to setversion and setversion-release so that they can be used in repos with different layouts.
    * Added a function to read the version from pom.xml. This will be used to rollover the version after release.
    * Removed npm-reversion since it is unused.

--- bin/setversion-release.js
+++ bin/setversion-release.js
@@ -3,27 +3,17 @@
 const { existsSync } = require('fs');
 const { cwd } = require('process');
 const { join } = require('path');
-const { major, minor, patch } = require('semver');
-const { getVersion, setVersion } = require('../lib/version');
+const { getManifestVersion, getVersionWithoutPrerelease, setManifestVersion } = require('../lib/version');
 
 // Get the version without prerelease identifier, e.g. "1.2.3-rc.4" -> "1.2.3"
-const currentVersion = getVersion(join(cwd(), 'package.json'));
-const version = stripPrerelease(currentVersion);
+const currentVersion = getManifestVersion(join(cwd(), 'package.json'));
+const version = getVersionWithoutPrerelease(currentVersion);
 
 console.log(version);
 
-const libManifest = join(cwd(), 'dist', 'library', 'package.json');
-
-// Update the version in the build manifest files
-if (existsSync(libManifest)) {
-    setVersion(libManifest, version);
-} else {
-    setVersion(join(cwd(), 'package.json'), version);
-    setVersion(join(cwd(), 'dist', 'package.json'), version);
-}
-
-
-
-function stripPrerelease(v) {
-    return `${major(v)}.${minor(v)}.${patch(v)}`;
-}
+// update the versions in all locations provided on the command line
+argv.slice(2).forEach(arg => {
+    if (existsSync(arg)) {
+        setManifestVersion(arg, version);
+    }
+});
