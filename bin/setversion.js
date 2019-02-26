7f77f2942dbfa5b813d2b839227e807e81d3e9d1
donna-marijne <donnamarijne+gh@gmail.com>
2019-02-26 10:14:23 +0000

    Refactoring scripts for quantum-ux-aspects (#3)
    * Added package-artifactory script which is adapted from the one in ux-aspects-darwin.
    * Added command line args to setversion and setversion-release so that they can be used in repos with different layouts.
    * Added a function to read the version from pom.xml. This will be used to rollover the version after release.
    * Removed npm-reversion since it is unused.

--- bin/setversion.js
+++ bin/setversion.js
@@ -1,34 +1,28 @@
 #!/usr/bin/env node
 
-const { env, cwd, exit } = require('process');
-const { join } = require('path');
-const { setVersion } = require('../lib/version');
+const { argv, env, exit } = require('process');
+const { existsSync } = require('fs-extra');
+const { getPomVersion, setManifestVersion } = require('../lib/version');
 
-const regex = /(\d+)\.(\d+)\.(\d+)\.(\d+)/;
+(async () => {
 
-// extract the version from the environment variable
-const version = toSemVer(env.VERSION);
+    // extract the version from the environment variable or pom.xml
+    const version = env.VERSION || await getPomVersion('pom.xml');
 
-console.log(version);
+    console.log(version);
 
-// if there is no version specified then throw an error
-if (!version) {
-    throw new Error('Version has not been specified!');
-}
-
-// update the versions in all locations
-setVersion(join(cwd(), 'package.json'), version);
-setVersion(join(cwd(), 'package-lock.json'), version);
-setVersion(join(cwd(), 'src', 'package.json'), version);
-
-// end the script
-exit();
-
-function toSemVer(version) {
-    const m = regex.exec(version);
-    if (m) {
-        return `${m[1]}.${m[2]}.${m[3]}+build.${m[4]}`;
+    // if there is no version specified then throw an error
+    if (!version) {
+        throw new Error('Version has not been specified!');
     }
 
-    return version;
-}
+    // update the versions in all locations provided on the command line
+    argv.slice(2).forEach(arg => {
+        if (existsSync(arg)) {
+            setManifestVersion(arg, version);
+        }
+    });
+
+    exit();
+
+})().catch(err => console.error(err, err.stack));
